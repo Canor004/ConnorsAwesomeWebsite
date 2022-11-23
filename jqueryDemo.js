30955bbc43b7629445082426cd17351d30cfc004
Canor004 <connor.mackay@shaw.ca>
2022-11-23 13:57:08 -0800

    took away jquery stuff, and commented it out

--- jqueryDemo.js
+++ jqueryDemo.js
@@ -1,7 +1,22 @@
-$(document).ready( () => {
+/*$(document).ready( () => {
     const $showpicture = $('#showMe');
     const $showButton = $('#showButton');
+    $showpicture.hide();
     $showButton.on('click', () => {
-        $showpicture.show();
+        $showpicture.slideDown();
     })
 })
+
+function loadDoc(){
+    console.log(document.getElementById("ajaxTest"));
+    const rq = new XMLHttpRequest();
+    rq.onreadystatechange = function() {
+        console.log(this.readyState);
+            if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("ajaxTest").innerHTML = this.responseText;
+            }
+        }
+    rq.open('GET', 'something.txt', true);
+    rq.send();
+};
+*/
