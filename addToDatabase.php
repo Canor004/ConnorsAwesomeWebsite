1f8c25eadde296bd4e59018c9098bf05c1f6b06e
Canor004 <connor.mackay@shaw.ca>
2022-12-12 10:10:54 -0800

    polished addToDatabase and maybe fixed sqlDemo

--- addToDatabase.php
+++ addToDatabase.php
@@ -1,19 +1,25 @@
 <?php
-    //$mysqli= mysqli_connect("host", "user", "password", "connorsAwesomeDatabase");
+    $mysqli= mysqli_connect("localhost", "cmmack", "letmein", "connorsAwesomeDatabase");
     $email = filter_input(INPUT_POST, "email");
     $fname = filter_input(INPUT_POST, "fname");
     $lname = filter_input(INPUT_POST, "lname");
     $tel = filter_input(INPUT_POST, "tel");
-    //$insertQuery = "INSERT INTO accounts VALUES ('".$fname."', '".$lname."', '".$email."', '".$tel."')";
-    //$result = mysqli_query($mysqli, $insertQuery);
+
+    $fname = "bill";
+    $lname = "clinton";
+    $email = "billyc@UnorthodoxRabi.ca";
+    $tel = "111-111-1234";
+    $add = "INSERT INTO accounts VALUES ('".$fname."', '".$lname."', '".$email."', '".$tel."')";
+    echo "<p>Query: ".$add."</p>";
+    $result = mysqli_query($sql, $add);
+    echo "<p>finished</p>";
+    
     echo "<html>";
-    echo "<head>";
-    echo "<link rel='stylesheet' href='index.css'>";
-    echo "</head>";
     echo "<body>";
     echo "<h4>New record created succesfully.</h4>";
     echo "<table>";
-    echo "<th>First</th><th>Last</th><th>Email</th><th>Phone</th>";
-    echo "<td>".$fname."</td><td>".$lname."</td><td>".$email."</td><td>".$tel."</td>";
+    echo "<tr><th>First</th><th>Last</th><th>Email</th><th>Phone</th></tr>";
+    echo "<tr><td>".$fname."</td><td>".$lname."</td><td>".$email."</td><td>".$tel."</td></tr>";
     echo "</table>";
+    echo "<p>connected</p>";
 ?> 
